e6315b24e9e68f0e3e03dfcdc0e2927d17a8af3c
NurlatifatusholihahRamadhan <149514063+NurlatifatusholihahRamadhan@users.noreply.github.com>
2023-12-10 21:05:46 +0700

    Add files via upload
    nurlatifatusolihah
    0110123242
    SI01

---
+++ aritmatika.py
@@ -0,0 +1,41 @@
+import math
+def penjumlahan(bil1, bil2):
+    hasil = bil1 + bil2
+    print("hasil penjumlahan: ", hasil)
+
+def pengurangan(bil1, bil2):
+    hasil = bil1 - bil2
+    print("hasil pengurangan: ", hasil)
+
+def perkalian(bil1, bil2):
+    hasil = bil1 * bil2
+    print("hasil kalian: ", hasil)
+
+def pembagian(bil1, bil2):
+    hasil = bil1 / bil2
+    print("hasil bagi: ", hasil)
+
+def pangkat(bil1, bil2):
+    hasil = bil1 ** bil2
+    print("hasil pangkat: ", hasil)
+
+def log(bil1):
+    hasil = math.log(bil1)
+    print("hasil log: ", hasil)
+
+def akar(bil1):
+    hasil = math.sqrt(bil1)
+    print("hasil akar: ", hasil)
+
+def sin(bil1):
+    hasil = math.sin(math.radians(bil1))
+    print("hasil sin: ", hasil)
+
+def cos(bil1):
+    hasil = math.cos(math.radians(bil1))
+    print("hasil cos: ", hasil)
+
+def tan(bil1):
+    hasil = math.tan(bil1)
+    print("hasil tan: ", hasil)
+
